924e14b8d82eac892b022b57e72f80a5d6367902
H.G.D. IV <20808723+Nothingrhymeswithorange@users.noreply.github.com>
2019-10-17 17:14:05 -0400

    Added DBhelper. Started on list_item.xml.

---
+++ app/src/main/java/com/ethical_techniques/notemaker/DBHelper.java
@@ -0,0 +1,40 @@
+package com.ethical_techniques.notemaker;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "localMyNotes.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String CREATE_TABLE_NOTE =
+            "create table note (_id integer primary key autoincrement, "
+                    + "noteContent text not null, subject text);";
+
+
+    public DBHelper(Context context) {
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(CREATE_TABLE_NOTE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(DBHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will alter table data");
+            db.execSQL("DROP TABLE IF EXISTS note");
+        onCreate(db);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/ethical_techniques/notemaker/NoteAdapter.java
@@ -0,0 +1,88 @@
+package com.ethical_techniques.notemaker;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends ArrayAdapter<Note> {
+
+
+        private ArrayList<Note> items;
+        private Context adapterContext;
+
+        public NoteAdapter(Context context, ArrayList<Note> items){
+            super(context, R.layout.list_item,items);
+            adapterContext = context;
+            this.items = items;
+        }
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent){
+            View v = convertView;
+
+            try {
+                Note note = items.get(position);
+
+                if (v ==  null){
+                    LayoutInflater vi = (LayoutInflater)
+                            adapterContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    v = vi.inflate(R.layout.list_item, null);
+                }
+
+
+            }catch(Exception e3){
+                e3.printStackTrace();
+                e3.getCause();
+            }
+            return v;
+        }
+        public void showDelete(final int position, final View convertView,
+                               final Context context, final Note note){
+            View v = convertView;
+            final Button b = v.findViewById(R.id.buttonDeleteNote);
+            if(b.getVisibility() == View.INVISIBLE) {
+                b.setVisibility(View.VISIBLE);
+                b.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        hideDelete(position,convertView,context);
+                        items.remove(note);
+                        deleteOption(note.getNoteID(),context);
+
+
+                    }
+                });
+            }else{
+                hideDelete(position,convertView,context);
+            }
+
+        }
+        private void deleteOption(int noteToDelete, Context context){
+            NoteDataSource db = new NoteDataSource(context);
+            try{
+                db.open();
+                db.delete(noteToDelete);
+                db.close();
+            }catch (Exception e){
+                Toast.makeText(adapterContext,"Delete contact failed", Toast.LENGTH_LONG).show();
+            }
+            this.notifyDataSetChanged();
+
+        }
+        public void hideDelete(int position,View convertView, Context context) {
+            View v = convertView;
+            final Button b = (Button) v.findViewById(R.id.buttonDeleteNote);
+            b.setVisibility(View.INVISIBLE);
+            b.setOnClickListener(null);
+        }
+    }
+}
